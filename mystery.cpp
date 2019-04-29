b72abe375d557c325a1ee073823ea1bae15dc228
IIITBIZZLEIII <IIITBIZZLEIII@users.noreply.github.com>
2019-04-29 12:06:24 -0400

    update

--- mystery.cpp
+++ mystery.cpp
@@ -14,7 +14,7 @@ void mystery1(auto& Data)
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
-  for ( int i = 0 ; i < Data.size( ) ; i++)
+  for (int i = 0 ; i < Data.size( ) ; i++)
   {
     for ( int j = 0 ; j < i ; j++)
 	if ( Data[ i ] < Data[ j ] )
@@ -26,6 +26,48 @@ void mystery1(auto& Data)
 }
 
 //... Other mysteries...
+void mystery2(auto& Data)
+{
+	cout<<endl<<"Mystery 2"<<endl<<endl;
+	int x, y, unsort, tmp;
+	for (x = 0; x < Data.size()-1; x++)
+	{
+		unsort = x;
+		for (y = x + 1; y < Data.size(); y++)
+		{
+			if(Data[y] < Data[unsort])
+				unsort = y;
+		}
+		if (unsort != x)
+		{
+			tmp = Data[x];
+			Data[x] = Data[unsort];
+			Data[unsort] = tmp;
+		}
+	print(Data);
+	}
+}
+
+void mystery3(auto& Data)
+{
+	cout<<endl<<"Mystery 3"<<endl<<endl;
+	int nxtIdex, move, instVal;
+	
+	for (nxtIdex = 1; nxtIdex < Data.size(); nxtIdex++)
+	{
+		instVal = Data[nxtIdex];
+		move = nxtIdex;
+		
+		while(move > 0 && Data[move - 1] > instVal)
+		{
+			Data[move] = Data[move - 1];
+			move--;
+		}
+		Data[move] = instVal;
+	print(Data);
+	}
+	
+}
 
 int main()
 {
